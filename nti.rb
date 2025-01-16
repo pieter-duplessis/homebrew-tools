74cbccf75f06820a0f26ca3c60d839a493e898c8
Pieter du Plessis <pietertpt@gmail.com>
2025-01-16 21:42:12 +0200

    Add formula for nti

---
+++ nti.rb
@@ -0,0 +1,16 @@
+class MyProject < Formula
+  desc "A description of your Rust project"
+  homepage "https://github.com/pieter-duplessis/homebrew-tools"
+  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/v0.3.0/nti_silicon.tar.gz"
+  sha256 "89cb9c615487851335429edcf5ce2f73942ee3b4fbd38561884bd89b4947903c"
+  version "0.3.0"
+  license "GNU AGPLv3"
+
+  def install
+    bin.install "nti"
+  end
+
+  test do
+    system "#{bin}/nti", "--version"
+  end
+end
