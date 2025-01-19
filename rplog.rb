7d7949d97daa2bb18e6d08ee2f1dd479b149609e
Pieter du Plessis <pietertpt@gmail.com>
2025-01-19 22:38:25 +0200

    Update RPLog File Name

--- rplog.rb
+++ rplog.rb
@@ -1,5 +1,5 @@
-class Nti < Formula
-  desc "Notification tool for sending email, sms, and push notifications"
+class Rplog < Formula
+  desc "A tool to help with procossing and analyzing logs"
   homepage "https://github.com/pieter-duplessis/homebrew-tools"
   url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/rplog-v0.2.0/rplog.tar.gz"
   sha256 "17e472bf554b51c71aba3344b1f9745cf810dca4609a511c5e9201ea5873fa0f"
