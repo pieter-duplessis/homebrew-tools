81f4e773a939f7385cc3624a0788179962d74a85
Pieter du Plessis <pietertpt@gmail.com>
2025-01-19 22:34:29 +0200

    Update For Tag Release

--- rplog.rb
+++ rplog.rb
@@ -1,7 +1,7 @@
 class Nti < Formula
   desc "Notification tool for sending email, sms, and push notifications"
   homepage "https://github.com/pieter-duplessis/homebrew-tools"
-  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/v0.2.0/rplog.tar.gz"
+  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/rplog-v0.2.0/rplog.tar.gz"
   sha256 "17e472bf554b51c71aba3344b1f9745cf810dca4609a511c5e9201ea5873fa0f"
   version "0.2.0"
   license "GNU AGPLv3"
