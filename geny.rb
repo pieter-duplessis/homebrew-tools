ed414a646e68164fe33d46f1915ac3834e7eea1d
Pieter du Plessis <pietertpt@gmail.com>
2025-04-14 08:55:18 +0200

    Fix Geny Formula Name

--- geny.rb
+++ geny.rb
@@ -1,4 +1,4 @@
-class Rplog < Formula
+class Geny < Formula
   desc "A tool that will help generate code for your project based off of template files"
   homepage "https://github.com/pieter-duplessis/homebrew-tools"
   url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/geny-v0.2.0/geny.tar.gz"
