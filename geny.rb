5d1c7417972c594c53816cd37c762102061724b8
Pieter du Plessis <pietertpt@gmail.com>
2025-04-19 09:38:15 +0200

    Update geny.rb

--- geny.rb
+++ geny.rb
@@ -1,9 +1,9 @@
 class Geny < Formula
   desc "A tool that will help generate code for your project based off of template files"
   homepage "https://github.com/pieter-duplessis/homebrew-tools"
-  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/geny-v0.2.2/geny.tar.gz"
-  sha256 "4ca7e14cfd23f3534b8bdbd76db289e2780d7669b50cff884c790402e59546ef"
-  version "0.2.2"
+  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/geny-v0.2.3/geny.tar.gz"
+  sha256 "6722886ea52f6e53cd0820edb8099c12f4b8b57dc1eb9e50b4af3e89588644a0"
+  version "0.2.3"
   license "Apache 2.0"
 
   def install
