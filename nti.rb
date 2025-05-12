68c03e0702a2ccc078e35dd2c42e2dd2ed9bb3c9
Pieter du Plessis <pietertpt@gmail.com>
2025-05-12 09:38:37 +0200

    Update nti.rb

--- nti.rb
+++ nti.rb
@@ -1,9 +1,9 @@
 class Nti < Formula
   desc "Notification tool for sending email, sms, and push notifications"
   homepage "https://github.com/pieter-duplessis/homebrew-tools"
-  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/v0.3.6/nti_aarch64-apple.tar.gz"
-  sha256 "a39f21d4e64485960c6687cb07d14e1ed4b532e7df57087de79d924941d11c22"
-  version "0.3.6"
+  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/nti-v0.3.7/nti_aarch64-apple.tar.gz"
+  sha256 "f9643e26745a908fa279d18a4c4c18d6f0f7e88fa843e99a82f1f30161cc2c2f"
+  version "0.3.7"
   license "GNU AGPLv3"
 
   def install
