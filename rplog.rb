7773ed945e4f21302c06994d40c04d24f2f9fe73
Pieter du Plessis <pietertpt@gmail.com>
2025-05-13 22:33:29 +0200

    Update rplog.rb

--- rplog.rb
+++ rplog.rb
@@ -1,9 +1,9 @@
 class Rplog < Formula
   desc "A tool to help with procossing and analyzing logs"
   homepage "https://github.com/pieter-duplessis/homebrew-tools"
-  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/rplog-v0.2.0/rplog.tar.gz"
-  sha256 "17e472bf554b51c71aba3344b1f9745cf810dca4609a511c5e9201ea5873fa0f"
-  version "0.2.0"
+  url "https://github.com/pieter-duplessis/homebrew-tools/releases/download/rplog-v0.2.1/rplog.tar.gz"
+  sha256 "554746a72c2dd910340e6cdec17683d845f3edca13af05c7b0228cbec35a8232"
+  version "0.2.1"
   license "GNU AGPLv3"
 
   def install
